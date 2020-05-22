09c99d94ba9440e4d52e2c6037deca8c183304cd
johnatanbrayan <johnatanbrayan1@gmail.com>
2020-05-21 21:35:54 -0300

    Validacao e tratamento de excecao para emails repetidos no processo de criacao na entidade cliente

--- src/main/java/br/com/johnatanbrayan/repository/ClienteRepository.java
+++ src/main/java/br/com/johnatanbrayan/repository/ClienteRepository.java
@@ -1,7 +1,12 @@
 package br.com.johnatanbrayan.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.johnatanbrayan.domain.Cliente;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Long>{}
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
+}
--- src/main/java/br/com/johnatanbrayan/service/validation/ClienteInsertValidation.java
+++ src/main/java/br/com/johnatanbrayan/service/validation/ClienteInsertValidation.java
@@ -6,18 +6,33 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.johnatanbrayan.domain.Cliente;
 import br.com.johnatanbrayan.domain.dto.ClienteNewDTO;
 import br.com.johnatanbrayan.domain.enums.TipoCliente;
+import br.com.johnatanbrayan.repository.ClienteRepository;
 import br.com.johnatanbrayan.resources.exception.FieldMessage;
 import br.com.johnatanbrayan.service.validation.utils.BR;
 
 public class ClienteInsertValidation implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
 	@Override
 	public void initialize(ClienteInsert ann) {}
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
 		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existe!!"));
+		}
 		
 		if(objDto.getTipoCliente().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj","CPF inválido"));
@@ -32,6 +47,7 @@ public class ClienteInsertValidation implements ConstraintValidator<ClienteInser
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
 					.addConstraintViolation();
 		}
+		
 		return list.isEmpty();
 	}
 }
